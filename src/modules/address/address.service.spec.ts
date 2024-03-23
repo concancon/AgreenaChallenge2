54b44a27abdab9fda1ea4bbd4812f7f8393caa49
Camilo Ocampo <comangus@gmail.com>
2024-03-23 11:55:24 +0100

    Setup mock for address service

--- src/modules/address/address.service.spec.ts
+++ src/modules/address/address.service.spec.ts
@@ -1,30 +1,13 @@
-//jest.mock("@googlemaps/google-maps-services-js");
-import config from "config/config";
 import { AddressService } from "./address.service";
-import type * as OriginalGoogleMapsServices from "@googlemaps/google-maps-services-js";
+import { when } from "jest-when";
+import config from "config/config";
+let mock: jest.Mock;
+
 jest.mock("@googlemaps/google-maps-services-js", () => {
-  const originalModule = jest.requireActual<typeof OriginalGoogleMapsServices>("@googlemaps/google-maps-services-js");
   return {
     __esModule: true,
-    ...originalModule,
     Client: class {
-      public geocode(args: { params: { key: string; address: string } }) {
-        if (args.params.key === config.GOOGLE_MAPS_API_KEY && typeof args.params.address === "string")
-          return {
-            data: {
-              results: [
-                {
-                  geometry: {
-                    location: {
-                      lat: 42,
-                      lng: 42,
-                    },
-                  },
-                },
-              ],
-            },
-          };
-      }
+      public geocode = mock;
     },
   };
 });
@@ -40,10 +23,16 @@ describe("AddressService", () => {
 
   describe(".getCoordinatesFromAddress", () => {
     it("should create new farm", async () => {
+      const inputAddress: string = "Andersenstr. 3 10439";
+      const response = { data: { results: [{ geometry: { location: { lat: 42, lng: 42 } } }] } };
+      mock = jest.fn();
+      when(mock)
+        .calledWith({ params: { key: config.GOOGLE_MAPS_API_KEY, address: inputAddress } })
+        .mockReturnValue(response);
+
       addressService = new AddressService();
 
-      const input: string = "Andersenstr. 3 10439";
-      const { lat, lng } = await addressService.getCoordinatesFromAddress(input);
+      const { lat, lng } = await addressService.getCoordinatesFromAddress(inputAddress);
       expect(lat).toBe(42);
       expect(lng).toBe(42);
     });
