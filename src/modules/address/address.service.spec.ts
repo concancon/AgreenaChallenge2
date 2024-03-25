3fb1bdb32bc60ab4d83174bab19f3768e9bc555c
Camilo Ocampo <comangus@gmail.com>
2024-03-25 09:58:06 +0100

    React to geocode service failure

--- src/modules/address/address.service.spec.ts
+++ src/modules/address/address.service.spec.ts
@@ -1,6 +1,9 @@
 import { AddressService } from "./address.service";
 import { when } from "jest-when";
 import config from "config/config";
+import { AxiosError, AxiosResponse } from "axios";
+import { BadRequestError, UnauthorizedError } from "errors/errors";
+
 let mock: jest.Mock;
 
 jest.mock("@googlemaps/google-maps-services-js", () => {
@@ -24,17 +27,64 @@ describe("AddressService", () => {
   describe(".getCoordinatesFromAddress", () => {
     it("should return coordinates when passed the correct api key and the provided address", async () => {
       const inputAddress: string = "Andersenstr. 3 10439";
-      const response = { data: { results: [{ geometry: { location: { lat: 42, lng: 42 } } }] } };
+      const expectedResponse = { data: { results: [{ geometry: { location: { lat: 42, lng: 42 } } }] } };
       mock = jest.fn();
       when(mock)
         .calledWith({ params: { key: config.GOOGLE_MAPS_API_KEY, address: inputAddress } })
-        .mockReturnValue(response);
+        .mockReturnValue(expectedResponse);
 
       addressService = new AddressService();
 
       const { lat, lng } = await addressService.getCoordinatesFromAddress(inputAddress);
-      expect(lat).toBe(42);
-      expect(lng).toBe(42);
+      expect(lat).toBe(expectedResponse.data.results[0].geometry.location.lat);
+      expect(lng).toBe(expectedResponse.data.results[0].geometry.location.lng);
+    });
+
+    it("should throw a bad reqeuest error when passed the correct api key but an invalid address", async () => {
+      const inputAddress: string = "Invalid address";
+      const expectedResponse = { data: { results: [] } };
+      mock = jest.fn();
+      when(mock)
+        .calledWith({ params: { key: config.GOOGLE_MAPS_API_KEY, address: inputAddress } })
+        .mockReturnValue(expectedResponse);
+
+      addressService = new AddressService();
+
+      await addressService.getCoordinatesFromAddress(inputAddress).catch((error: BadRequestError) => {
+        expect(error).toBeInstanceOf(BadRequestError);
+        expect(error.message).toBe("Address not found");
+      });
+    });
+
+    it("should throw an unauthorized error when passed an invalid API key", async () => {
+      config.GOOGLE_MAPS_API_KEY = "now im wrong";
+      const inputAddress: string = "Invalid address";
+      mock = jest.fn();
+      when(mock)
+        .calledWith({ params: { key: config.GOOGLE_MAPS_API_KEY, address: inputAddress } })
+        .mockRejectedValue(new AxiosError(undefined, undefined, undefined, null, { status: 403 } as AxiosResponse));
+
+      addressService = new AddressService();
+
+      await addressService.getCoordinatesFromAddress(inputAddress).catch((error: UnauthorizedError) => {
+        expect(error).toBeInstanceOf(UnauthorizedError);
+      });
+    });
+    it("should throw an error when call failed for any reason", async () => {
+      config.GOOGLE_MAPS_API_KEY = "could be right or wrong";
+      const inputAddress: string = "could be right or wrong";
+      mock = jest.fn();
+      const expectedMessage = "something awful happened";
+      when(mock)
+        .calledWith({ params: { key: config.GOOGLE_MAPS_API_KEY, address: inputAddress } })
+        .mockRejectedValue(new AxiosError(expectedMessage, undefined, undefined, null, { status: 500 } as AxiosResponse));
+
+      addressService = new AddressService();
+
+      await addressService.getCoordinatesFromAddress(inputAddress).catch((error: UnauthorizedError) => {
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toBe(expectedMessage);
+      });
     });
   });
 });
--- src/modules/address/address.service.ts
+++ src/modules/address/address.service.ts
@@ -1,6 +1,8 @@
 import { EnvironmentVariables } from "config/environment-variables";
-import { Client } from "@googlemaps/google-maps-services-js";
+import { Client, GeocodeResponse } from "@googlemaps/google-maps-services-js";
 import config from "config/config";
+import { BadRequestError, UnauthorizedError } from "errors/errors";
+import { AxiosError } from "axios";
 
 export class AddressService {
   private readonly googleMapsClient: Client;
@@ -11,9 +13,22 @@ export class AddressService {
   }
 
   public async getCoordinatesFromAddress(address: string): Promise<{ lat: number; lng: number }> {
-    const response = await this.googleMapsClient.geocode({
-      params: { key: this.config.GOOGLE_MAPS_API_KEY, address },
-    });
+    let response: GeocodeResponse;
+    try {
+      response = await this.googleMapsClient.geocode({
+        params: { key: this.config.GOOGLE_MAPS_API_KEY, address },
+      });
+    } catch (e: any) {
+      const error = e as AxiosError;
+      if (error.response?.status === 403) throw new UnauthorizedError();
+      else {
+        throw new Error(error.message);
+      }
+    }
+    if (!response.data.results.length) {
+      throw new BadRequestError("Address not found");
+    }
+
     const { lat, lng } = response.data.results[0].geometry.location;
     return { lat, lng };
   }
