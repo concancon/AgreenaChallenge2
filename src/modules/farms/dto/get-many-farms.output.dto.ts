4869d328f4214f92ea4d534746ef47876c2db994
Camilo Ocampo <comangus@gmail.com>
2024-04-03 16:56:59 +0200

    Add openapi documentation

--- src/modules/farms/dto/get-many-farms.output.dto.ts
+++ src/modules/farms/dto/get-many-farms.output.dto.ts
@@ -1,6 +1,15 @@
 import { Expose } from "class-transformer";
 import { Farm } from "../entities/farm.entity";
-
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    GetManyFarmsOutputDto:
+ *      type: object
+ *      properties:
+ *        farms:
+ *          type: object
+ */
 export class GetManyFarmsOutputDto {
   constructor(partial?: Partial<GetManyFarmsOutputDto>) {
     Object.assign(this, partial);
